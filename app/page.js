c17ecaa37a7a98000990341fcd9eba6ac0b05061
user.name like: "zeeshan <user.email like: "zeeshan484anwar@gmail.com>
2025-02-26 16:41:55 +0500

    Fix: Corrected InputBox component filename casing

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from 'react'
-import InputBox from './components/Inputbox'
+import InputBox from './components/InputBox'
 import useCurrencyInfo from './hooks/usecurrencyinfo'
 
 const defaultCurrencies = ["USD", "EUR", "GBP", "INR", "PKR"] // Default options for initial render
